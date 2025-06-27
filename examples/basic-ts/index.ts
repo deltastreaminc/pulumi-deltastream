d5a46b178761c13766c2daea47e12faf01e0f970
Krishna Raman <kraman@deltastream.io>
2025-06-26 18:17:56 -0700

    fix nodejs database test

--- examples/basic-ts/index.ts
+++ examples/basic-ts/index.ts
@@ -1,39 +1,23 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as deltastream from "@deltastream/pulumi";
 import { randomUUID } from "crypto";
+import { awaitStackRegistrations } from "@pulumi/pulumi/runtime";
 
 const config = new pulumi.Config();
 const apiKey = config.require("DS_API_KEY");
 const orgID = config.require("DS_ORGANIZATION_ID");
 const serverUri = config.require("DS_SERVER_URI");
 
-const provider = new deltastream.Provider("deltastream", {
-    apiKey: apiKey,
-    organization: orgID,
-    server: serverUri,
-});
+export = async () => {
+    const provider = new deltastream.Provider("deltastream", {
+        apiKey: apiKey,
+        organization: orgID,
+        server: serverUri,
+    });
 
-const dbName = pulumi.interpolate`pulumi-ts-db-${randomUUID()}`;
-
-(async() => {
     const db = new deltastream.Database("db", {
-        name: dbName,
+        name: `pulumi-ts-database`,
     }, { 
         provider: provider,
-    });
-    const dbOutputName = (await db.name).get()
-
-    const dbList = await deltastream.getDatabases({
-        provider: provider,
-    });
-
-    let dbExists = false;
-    for (const db of dbList.items) {
-        if (dbOutputName === db.name) {
-            dbExists = true;
-        }
-    }
-    if (!dbExists) {
-        throw new Error(`Database ${dbName} does not exist.`);
-    }
-})();
+    })
+}
--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -1,6 +1,6 @@
 // Copyright 2024, Pulumi Corporation.  All rights reserved.
-// g o:build nodejs || all
-// + build nodejs all
+//go:build nodejs || all
+// +build nodejs all
 
 package examples
 
